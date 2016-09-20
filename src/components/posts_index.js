35b2d7a1c4638d35b340c03ebb8f49d054751b57
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-20 17:04:13 +0300

    Promote posts component to container

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React , { Component } from 'react';
+import { connect } from 'react-redux';
+//import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>
-};
+class PostsIndex extends Component {
+  componentWillMount(){
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>List of blog posts.</div>
+    );
+  }
+}
+
+// function mapDispatchToProps(dispatch){
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+//export default connect(null, mapDispatchToProps)(PostsIndex);
+
+//refactor instead of 3 line of code so we dont need bindActionCreators
+//at the top
+export default connect(null, {fetchPosts})(PostsIndex);
