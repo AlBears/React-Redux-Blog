e710750a676568f04b05326676fc4deefa62278d
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-21 10:38:53 +0300

    Link implementation

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
-//import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount(){
@@ -9,16 +9,15 @@ class PostsIndex extends Component {
   }
   render() {
     return (
-      <div>List of blog posts.</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of blog posts.
+      </div>
     );
   }
 }
-
-// function mapDispatchToProps(dispatch){
-//   return bindActionCreators({ fetchPosts }, dispatch);
-// }
-//export default connect(null, mapDispatchToProps)(PostsIndex);
-
-//refactor instead of 3 line of code so we dont need bindActionCreators
-//at the top
 export default connect(null, {fetchPosts})(PostsIndex);
